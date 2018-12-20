25cdcb358bba2394509fe92110ed4eabfcea75fd
Pavle Jonoski <pavle.jonoski@keitaro.com>
2018-12-20 10:45:37 +0100

    Added ignore patterns for JSON and YAML.

--- tools/keystore.go
+++ tools/keystore.go
@@ -100,7 +100,7 @@ func NewDirKeyStore(keysDir string) (KeyStore, error) {
 	for _, file := range files {
 		if !file.IsDir() {
 			name := file.Name()
-			if suffix := hasAnySuffix(name, ".pub", ".pubk", ".pk", ".cert", ".crt"); suffix == nil {
+			if suffix := hasAnySuffix(name, ".pub", ".pubk", ".pk", ".cert", ".crt", ".json", ".yaml", ".yml"); suffix == nil {
 				keysMap[name] = path.Join(keysDir, name)
 			}
 		}
